5c09371905ba736e3eafa4f12deaceabc00369c9
Blizzer Softword <eddyeddy1800@gmail.com>
2022-11-07 15:42:35 -0500

    Actualizado las statement de las tablas

--- main.py
+++ main.py
@@ -17,25 +17,25 @@ def start_db():
     db = dbm.createDatabase()   # Crear la base de datos
     ## <Tablas>
     #   Usuarios
-    dbm.createTable(db, "id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT", "title TEXT NOT NULL", "name TEXT NOT NULL", "lnacimiento INTEGER NOT NULL", "edad INTEGER NOT NULL", "raza INTEGER NOT NULL", "padre INTEGER NOT NULL", "madre INTEGER NOT NULL", "sexo INTEGER NOT NULL", "renombre INTEGER NOT NULL", "nivel INTEGER NOT NULL", "vida REAL NOT NULL", "mana REAL NOT NULL", "mvida INTEGER NOT NULL", "mmana INTEGER NOT NULL", "xp REAL NOT NULL", "heridas INTEGER NOT NULL", "bwin INTEGER NOT NULL", "blose INTEGER NOT NULL", "batallas INTEGER NOT NULL", "tierras INTEGER NOT NULL", "clan INTEGER NOT NULL", "home INTEGER NOT NULL", "live INTEGER NOT NULL",t_name="Usuarios")
+    dbm.createTable(db, dbm.createCamp("id", dbm.INT, primary=True, ai=True), dbm.createCamp("title", dbm.TXT), dbm.createCamp("name", dbm.TXT), dbm.createCamp("lnacimiento", dbm.INT), dbm.createCamp("edad", dbm.INT), dbm.createCamp("raza", dbm.INT), dbm.createCamp("padre", dbm.INT), dbm.createCamp("madre", dbm.INT), dbm.createCamp("sexo", dbm.INT), dbm.createCamp("renombre", dbm.INT), dbm.createCamp("nivel", dbm.INT), "vida REAL NOT NULL", "mana REAL NOT NULL", dbm.createCamp("mvida", dbm.INT), dbm.createCamp("mmana", dbm.INT), "xp REAL NOT NULL", dbm.createCamp("heridas", dbm.INT), dbm.createCamp("bwin", dbm.INT), dbm.createCamp("blose", dbm.INT), dbm.createCamp("batallas", dbm.INT), dbm.createCamp("tierras", dbm.INT), dbm.createCamp("clan", dbm.INT), dbm.createCamp("home", dbm.INT), dbm.createCamp("live", dbm.INT),t_name="Usuarios")
     #   Clanes
-    dbm.createTable(db, "id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT", "name TEXT NOT NULL", "miembros INTEGER NOT NULL", t_name="Clanes")
+    dbm.createTable(db, dbm.createCamp("id", dbm.INT, primary=True, ai=True), dbm.createCamp("name", dbm.TXT), dbm.createCamp("miembros", dbm.INT), t_name="Clanes")
     #   Clases
-    dbm.createTable(db, "id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT", "name TEXT NOT NULL", t_name="Clases")
+    dbm.createTable(db, dbm.createCamp("id", dbm.INT, primary=True, ai=True), dbm.createCamp("name", dbm.TXT), t_name="Clases")
     #   Razas
-    dbm.createTable(db, "id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT", "name TEXT NOT NULL", t_name="Razas")
+    dbm.createTable(db, dbm.createCamp("id", dbm.INT, primary=True, ai=True), dbm.createCamp("name", dbm.TXT), t_name="Razas")
     #   Estructuras
-    dbm.createTable(db, "id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT", "name TEXT NOT NULL", "action INTEGER NOT NULL", t_name="Estructuras")
+    dbm.createTable(db, dbm.createCamp("id", dbm.INT, primary=True, ai=True), dbm.createCamp("name", dbm.TXT), dbm.createCamp("action", dbm.INT), t_name="Estructuras")
     #   Acciones
-    dbm.createTable(db, "id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT", "name TEXT NOT NULL", "action INTEGER NOT NULL", "args TEXT NOT NULL", t_name="Acciones")
+    dbm.createTable(db, dbm.createCamp("id", dbm.INT, primary=True, ai=True), dbm.createCamp("name", dbm.TXT), dbm.createCamp("action", dbm.INT), dbm.createCamp("args", dbm.TXT), t_name="Acciones")
     #   Items
-    dbm.createTable(db, "id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT", "name TEXT NOT NULL", "desc TEXT NOT NULL", "action INTEGER NOT NULL", "FOREIGN KEY(action) REFERENCES Acciones(id)",t_name="Items")
+    dbm.createTable(db, dbm.createCamp("id", dbm.INT, primary=True, ai=True), dbm.createCamp("name", dbm.TXT), dbm.createCamp("desc", dbm.TXT), dbm.createCamp("action", dbm.INT), "FOREIGN KEY(action) REFERENCES Acciones(id)",t_name="Items")
     #   Lugares
-    dbm.createTable(db, "id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT", "name TEXT NOT NULL", "dueño INTEGER NOT NULL",  "casas INTEGER NOT NULL", "jugadores INTEGER NOT NULL", "special INTEGER NOT NULL", t_name="Lugares")
+    dbm.createTable(db, dbm.createCamp("id", dbm.INT, primary=True, ai=True), dbm.createCamp("name", dbm.TXT), dbm.createCamp("dueño", dbm.INT),  dbm.createCamp("casas", dbm.INT), dbm.createCamp("jugadores", dbm.INT), dbm.createCamp("special", dbm.INT), t_name="Lugares")
     #   Inventory
-    dbm.createTable(db, "id INTEGER NOT NULL PRIMARY KEY", "items TEXT NOT NULL", "money INTEGER NOT NULL", "cabeza INTEGER NOT NULL", "pecho INTEGER NOT NULL", "muñecas INTEGER NOT NULL", "piernas INTEGER NOT NULL", "zapatos INTEGER NOT NULL", "mano_izq INTEGER NOT NULL", "mano_der INTEGER NOT NULL", "cuello INTEGER NOT NULL", "flechas INTEGER NOT NULL", t_name="Inventory")
+    dbm.createTable(db, dbm.createCamp("id", dbm.INT, primary=True), dbm.createCamp("items", dbm.TXT), dbm.createCamp("money", dbm.INT), dbm.createCamp("cabeza", dbm.INT), dbm.createCamp("pecho", dbm.INT), dbm.createCamp("muñecas", dbm.INT), dbm.createCamp("piernas", dbm.INT), dbm.createCamp("zapatos", dbm.INT), dbm.createCamp("mano_izq", dbm.INT), dbm.createCamp("mano_der", dbm.INT), dbm.createCamp("cuello", dbm.INT), dbm.createCamp("flechas", dbm.INT), t_name="Inventory")
     #   Players
-    dbm.createTable(db, "id INTEGER NOT NULL PRIMARY KEY", "tusern TEXT NOT NULL", "password TEXT NOT NULL", t_name="Players")
+    dbm.createTable(db, dbm.createCamp("id", dbm.INT, primary=True), dbm.createCamp("tusern", dbm.TXT), dbm.createCamp("password", dbm.TXT), t_name="Players")
     ## </Tablas>
     return db
 

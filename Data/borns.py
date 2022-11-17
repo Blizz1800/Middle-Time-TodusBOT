cb83f85bf00921b7301d2364dceade3183e53d72
Blizzer <eddyeddy1800@gmail.com>
2022-11-17 15:37:01 -0500

    Updated dbManager lib to delete data

--- Data/borns.py
+++ Data/borns.py
@@ -31,8 +31,13 @@ def generateBorn(db, idPadre, idMadre, childs=0):
     return
 
 
-def getKeys(db, id):
-    return dbm.getData(db, TABLE, CAMPS, f"`padre`={id} OR `madre`={id}", 0)
+def getKeys(db, id: int):
+    return dbm.getData(db, TABLE, CAMPS, f"`padre`={id} OR `madre`={id}")
+
+
+def clearKeys(db):
+    dbm.dropData(db, TABLE, "`childs`=0")
+
 
 def sha256(txt: str):
     txt = txt.encode('utf-8')
